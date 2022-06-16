87bee005c48a46c9938b748606fabb9c8279e517
Santiago Botta <sgobotta@gmail.com>
2022-06-16 13:12:10 -0300

    Implement args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+KAFKA_API_PORT=$1
+ADMIN_API_PORT=$2
+PANDA_PROXY_PORT=$3
+SCHEMA_REGISTRY_PORT=$4
+RPC_PORT=$5
+NODE_ID=$6
+
+HOSTNAME=redpanda-${NODE_ID}
+
 docker_run="docker run"
 
-run_redpanda="$docker_run -d -p 9092:9092 -p 9644:9644 docker.vectorized.io/vectorized/redpanda:${INPUT_VERSION} redpanda start --overprovisioned --smp 1 --memory 1G --reserve-memory 0M --node-id 0 --check=false"
+run_redpanda="$docker_run -d --hostname=${HOSTNAME} --net=redpandanet \ -p ${KAFKA_API_PORT}:9092 -p ${ADMIN_API_PORT}:9644 -p ${PANDA_PROXY_PORT}:8082 -p ${SCHEMA_REGISTRY_PORT}:8081 docker.vectorized.io/vectorized/redpanda:${INPUT_VERSION} redpanda start --overprovisioned --smp 1 --memory 1G --reserve-memory 0M --node-id ${NODE_ID} --kafka-addr INSIDE://0.0.0.0:29092,OUTSIDE://0.0.0.0:${KAFKA_API_PORT} --advertise-kafka-addr INSIDE://${HOSTNAME}:29092,OUTSIDE://localhost:${KAFKA_API_PORT} --pandaproxy-addr INSIDE://0.0.0.0:28082,OUTSIDE://0.0.0.0:${PANDA_PROXY_PORT} --advertise-pandaproxy-addr INSIDE://${HOSTNAME}:28082,OUTSIDE://localhost:#{PANDA_PROXY_PORT} --rpc-addr 0.0.0.0:${RPC_PORT} --advertise-rpc-addr ${HOSTNAME}:${RPC_PORT}"
 sh -c "$run_redpanda"
